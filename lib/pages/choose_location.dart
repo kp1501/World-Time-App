3660f6602d228d7cdafc9588e96567a232026ec2
kp1501 <67629838+kp1501@users.noreply.github.com>
2021-04-06 17:22:26 +0530

    Initial Commit

---
+++ lib/pages/choose_location.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:world_time/services/world_time.dart';
+
+class ChooseLocation extends StatefulWidget {
+  @override
+  _ChooseLocationState createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+
+  List<WorldTime> locations = [
+    WorldTime(location: 'India', flag: 'india.jpg', url: 'Asia/Kolkata'),
+    WorldTime(location: 'London', flag: 'uk.jpg', url: 'Europe/London'),
+    WorldTime(location: 'Chicago', flag: 'america.jpg', url: 'America/Chicago'),
+    WorldTime(location: 'New York', flag: 'america.jpg', url: 'America/New_York'),
+    WorldTime(location: 'Jakarta', flag: 'indonesia.jpg', url: 'Asia/Jakarta'),
+  ];
+
+  void updateTime(index) async{
+    WorldTime instance = locations[index];
+    await instance.getTime();
+    
+    Navigator.pop(context, {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
+      'isDayTime': instance.isDayTime,
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Choose a Location'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: ListView.builder(
+        itemCount: locations.length,
+        itemBuilder: (context, index){
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 1.0,horizontal: 5.0),
+            child: Card(
+              child: ListTile(
+                onTap: () {
+                  updateTime(index);
+                },
+                title: Text(locations[index].location),
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage('assets/${locations[index].flag}'),
+                ),
+              ),
+            ),
+          );
+        }
+      ),
+    );
+  }
+}
---
+++ lib/pages/home.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+
+  Map data = {};
+
+  @override
+  Widget build(BuildContext context) {
+
+    data = data.isNotEmpty ? data : ModalRoute.of(context).settings.arguments;
+    print(data);
+
+    String bgImage = data['isDayTime'] ? 'day.png' : 'night.png';
+    Color bgColor = data['isDayTime'] ? Colors.blue : Colors.indigo[700];
+
+    return Scaffold(
+      backgroundColor: bgColor,
+      body: SafeArea(
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/$bgImage'),
+              fit: BoxFit.cover,
+            )
+          ),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0,120,0,0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                FlatButton.icon(onPressed: () async {
+                  dynamic result = await Navigator.pushNamed(context, '/location');
+                  setState(() {
+                    data={
+                      'location': result['location'],
+                      'flag': result['flag'],
+                      'time': result['time'],
+                      'isDayTime': result['isDayTime']
+                    };
+                  });
+                  },icon: Icon(Icons.edit_location, color: Colors.grey[300],), label: Text('Edit Location',style: TextStyle(color: Colors.grey[300]),)),
+                SizedBox(height: 20.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      data['location'],
+                      style: TextStyle(
+                        fontSize: 28,letterSpacing: 2,
+                        color: Colors.white,
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(height: 20),
+                Text(data['time'],
+                style: TextStyle(
+                  fontSize: 66.0,
+                  color: Colors.white,
+                ),)
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
